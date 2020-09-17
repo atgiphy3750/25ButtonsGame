7b8ebc282152834499dfbf4e061f96d66c1fd3c8
plastic041 <plastic041@gmail.com>
2020-09-17 22:37:21 +0900

    Updates

---
+++ precache-manifest.35982a39728e5aa7876d3f67452606d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea82488d576f08d197d87e339b3a6c7",
+    "url": "/25ButtonsGame/index.html"
+  },
+  {
+    "revision": "9a6151a59ff00bbcbe8e",
+    "url": "/25ButtonsGame/static/css/main.4552d531.chunk.css"
+  },
+  {
+    "revision": "0218375cff6426ca75b0",
+    "url": "/25ButtonsGame/static/js/2.5431957a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/25ButtonsGame/static/js/2.5431957a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6151a59ff00bbcbe8e",
+    "url": "/25ButtonsGame/static/js/main.ead22467.chunk.js"
+  },
+  {
+    "revision": "800bd44be8e8abc5aba7",
+    "url": "/25ButtonsGame/static/js/runtime-main.e485f8c9.js"
+  }
+]);
